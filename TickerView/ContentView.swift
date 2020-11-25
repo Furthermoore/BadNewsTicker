adee52c59491c3742ec20f4acaee856cd9a540e3
Dan <dan.moore@vitalsource.com>
2020-11-25 01:04:09 -0500

    third commit

--- TickerView/ContentView.swift
+++ TickerView/ContentView.swift
@@ -11,48 +11,25 @@ import Combine
 struct ContentView: View {
     
     let badNewsScraper = BadNewsScraper()
-    
-    let rows = 16
-    let columns = 16
-    
-    @State private var activeColumn: Int = 7
-    
+            
     private let timer = Timer.publish(every: 0.05, on: .main, in: .common).autoconnect()
     
-    var body: some View {
-        Group {
-            ZStack {
-                VStack(spacing: 10) {
-                    ForEach((0..<rows).indices, id: \.self) { row in
-                        HStack(spacing: 10) {
-                            ForEach((0..<columns).indices, id: \.self) { column in
-                                Color.gray
-                            }
-                        }
-                    }
-                }
-                HStack(spacing: 10) {
-                    ForEach((0..<columns).indices, id: \.self) { column in
-                        VStack(spacing: 10) {
-                            if column == activeColumn {
-                                ForEach((0..<rows).indices, id: \.self) { row in
-                                    Color(Color.RGBColorSpace.sRGB, red: 0, green: 0.7, blue: 0, opacity: 1.0)
-                                }
-                            } else {
-                                ForEach((0..<rows).indices, id: \.self) { row in
-                                    Color.clear
-                                }
-                            }
-                        }
-                    }
-                }
+    init() {
+        for family: String in UIFont.familyNames
+        {
+            print(family)
+            for names: String in UIFont.fontNames(forFamilyName: family)
+            {
+                print("== \(names)")
             }
         }
-        .aspectRatio(contentMode: .fit)
-        .onReceive(timer, perform: { _ in
-            activeColumn = activeColumn == 0 ? columns - 1 : activeColumn - 1
-        })
     }
+    
+    var body: some View {
+        Text("You'll see")
+            .font(Font.custom("Digitaldream", size: 40))
+    }
+        
 }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
